e6dd5c2ef3d410b0895043102dbd6fcb772fe336
Tomás Piaggio <tomas.piaggio12@gmail.com>
2021-12-26 11:52:43 -0300

    Trying scripts on setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from distutils.core import setup
 setup(
     name='starmart',
     packages=['starmart'],
-    version='0.0.1',
+    version='0.0.2',
     license='apache-2.0',
     description='Starmart deployment tool',
     author='Tomas Piaggio',
     author_email='tomaspiaggio@starmart.io',
     url='https://starmart.io',
-    # download_url='https://github.com/starmart-io/starmart/archive/v_0.0.1.tar.gz',
     keywords=['AI', 'Machine Learning', 'Deep Learning', 'Serverless'],
     install_requires=[
         'GitPython==3.1.24',
@@ -19,7 +18,5 @@ setup(
         'halo==0.0.31'
     ],
     classifiers=[],
-    entrypoints={
-        'console_scripts': ['starmart=starmart.__main__:main']
-    }
+    scripts=['bin/starmart'],
 )
