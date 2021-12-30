4bb96fc501b265a19f9b6162c513c6ec01d5e15b
Tomas Piaggio <tomas.piaggio12@gmail.com>
2021-12-30 13:03:09 -0300

    Changed port on server

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name='starmart',
     packages=['starmart', 'starmart.server', 'starmart.config'],
-    version='0.0.18',
+    version='0.1.0',
     license='apache-2.0',
     description='Starmart deployment tool',
     author='Tomas Piaggio',
