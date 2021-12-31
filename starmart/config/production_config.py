609672546a621eafb404a04acb6b9ee4b1c22afb
Tomas Piaggio <tomas.piaggio12@gmail.com>
2021-12-31 12:17:13 -0300

    Bug on configuration

--- starmart/config/production_config.py
+++ starmart/config/production_config.py
@@ -3,7 +3,7 @@ from starmart.config.config import Config
 
 class ProductionConfig(Config):
     def github_repo(self) -> str:
-        return 'git@github.com:starmart-io/Model-Template.git'
+        return 'https://github.com/starmart-io/Model-Template.git'
 
     def authentication_host(self) -> str:
         return 'https://starmart.io'
